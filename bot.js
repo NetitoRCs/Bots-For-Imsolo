30162b560598038f045fa1cc75fb5db65088406c
NetitoRCs <77469863+NetitoRCs@users.noreply.github.com>
2021-04-05 14:35:24 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,25 +1,15 @@
-Browser.loadUrl("http://agar.io/");
-
-Browser.loadUrl("http://agar.io/");
+Browser.loadUrl("https://imsolo.pro/2017");
 Helper.sleep(10);
-
-Browser.loadUrl("http://agar.io/");
+Browser.loadUrl("https://imsolo.pro/2017");
 Browser.finishLoading();
-
-var click_script = "document.getElementById('play').click();";
+var click_script = "document.getElementById('play-btn').click();";
 Browser.executeJavscript(click_script);
-
-var screenshot = Browser.takeScreenshot();
-
-var template = new Image("play_button.png");
-
-var match = Vision.findMatch(screenshot, template, 0.95);
-
 var screenshot = Browser.takeScreenshot();
 var template = new Image("play_button.png");
-
 var match = Vision.findMatch(screenshot, template, 0.95);
-
+var screenshot = Browser.takeScreenshot();
+var template = new Image("play_button.png");
+var match = Vision.findMatch(screenshot, template, 0.95);
 if (match.isValid()) {
     var play_button_position = match.getRect().getCenter();
     Browser.leftClick(play_button_position);
