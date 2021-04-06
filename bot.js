fbafd52c57e3e6bf8856217e7a8f68169b9553b1
NetitoRCs <77469863+NetitoRCs@users.noreply.github.com>
2021-04-06 14:32:39 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,17 +1,4 @@
-Browser.loadUrl("https://imsolo.pro/2017");
-Helper.sleep(10);
-Browser.loadUrl("https://imsolo.pro/2017");
-Browser.finishLoading();
-var click_script = "document.getElementById('play-btn').click();";
-Browser.executeJavscript(click_script);
-var screenshot = Browser.takeScreenshot();
-var template = new Image("play_button.png");
-var match = Vision.findMatch(screenshot, template, 0.95);
-var screenshot = Browser.takeScreenshot();
-var template = new Image("play_button.png");
-var match = Vision.findMatch(screenshot, template, 0.95);
-if (match.isValid()) {
-    var play_button_position = match.getRect().getCenter();
-    Browser.leftClick(play_button_position);
-}
+function botStart() {
 
+botStart = get.ElementById("play-btn").click();
+    }
